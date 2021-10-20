8376852c6ba503f690df9429f1146f5ffb4671c2
Muhammedhashirm009 <87589712+Muhammedhashirm009@users.noreply.github.com>
2021-10-20 14:24:23 +0530

    Update jimbrootan.js

--- jimbrootan.js
+++ jimbrootan.js
@@ -13,9 +13,9 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 
  OA_AI: process.env.JINN_AI === undefined ? true : process.env.BOTNAME,  
- OB_NAME: process.env.BOT_NAME === undefined ? 'Jimbrootan' : process.env.BOT_NAME,
- OA_NAME: process.env.DEP_NAME === undefined ? 'Jimbrootan' : process.env.DEP_NAME,
- OA_REPLY: process.env.DEP_REPLY === undefined ? 'Mikhaiel is my creator' : process.env.DEP_REPLY,
+ OB_NAME: process.env.BOT_NAME === undefined ? 'HASHIRBOTV4' : process.env.BOT_NAME,
+ OA_NAME: process.env.DEP_NAME === undefined ? 'HASHIRBOTV4' : process.env.DEP_NAME,
+ OA_REPLY: process.env.DEP_REPLY === undefined ? ' *He is my creator* ' : process.env.DEP_REPLY,
     
 
 };
